d33495091a03d0e719456cb7fb46863ca603d671
corinz97 <jacopocorina@live.it>
2024-03-05 11:08:44 +0100

    !!Consegna finale!!, correzioni e modifica titolo elenco estratti

--- listings/ordering-rankings-Condorcet.kt
+++ listings/ordering-rankings-Condorcet.kt
@@ -5,7 +5,8 @@ for (competitor in condorcetFlattenedOrderInFinalRanking) {
     var listOfPositionsPerCompetitor: List<Int> = listOf()
     for (ranking in rankings.map { it.ranking.keys.toList() }) {
         var index = ranking.indexOfFirst { it.map { it.name }.contains(competitor) }
-        listOfPositionsPerCompetitor = listOfPositionsPerCompetitor + ++index
+        ++index
+        listOfPositionsPerCompetitor = listOfPositionsPerCompetitor + index
     }
     m = m + (competitor to listOfPositionsPerCompetitor)
 }
